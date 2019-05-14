dfe2d07f307da299d9d222ff72bc7587ca63e15f
Matthew Smith <matthew.t.smith@hotmail.com>
2019-05-14 14:37:22 -0500

    Finalized hyperparams with visualization of loss

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 # Normalizing function for dataset
 
@@ -19,17 +20,13 @@ def denorm(y, d):
 # Load the data
 data = pd.read_csv('./data/data.csv',
                    usecols=['Tmax(C)', 'Tmin(C)', 'RHmean(%)', 'Rain(mm)', 'Usage(kWh)'])
-data.fillna(0, inplace=True)
-data['RHmean(%)'] = data['RHmean(%)'] / 100
 dataset = data.values.astype(float)
-X = dataset[:, 0:4].round(2)
-Y = dataset[:, -1].round(2)
+X = dataset[:, 0:4]
+Y = dataset[:, 4]
 
 # Normalize the dataset
-normed_X = np.apply_along_axis(norm, 0, X)
+normed_X = np.apply_along_axis(norm, axis=0, arr=X)
 normed_Y = norm(Y)
-print(normed_X[0:10, :])
-print(normed_Y[0:10])
 
 # Split datasets into training and test
 split = 0.8
@@ -42,20 +39,42 @@ test_Y = normed_Y[val:]
 # Build the model with layers
 model = Sequential()
 model.add(Dense(4, input_dim=4, activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.25))
 model.add(Dense(4, activation='relu'))
-model.add(Dropout(0.4))
+model.add(Dropout(0.25))
 model.add(Dense(1))
 adam = Adam(lr=1e-5)
 model.compile(loss='mean_squared_error',
-              optimizer=adam, metrics=['mae', 'mse'])
-
+              optimizer=adam, metrics=['mean_absolute_error', 'mean_squared_error'])
 
 # Fit the model
 history = model.fit(train_X,
                     train_Y,
-                    epochs=100,
-                    validation_data=(test_X, test_Y))
+                    epochs=300,
+                    validation_data=(test_X, test_Y),
+                    verbose=0)
 model.save('energy.h5')
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+
+plt.figure()
+plt.xlabel('Epoch')
+plt.ylabel('Mean Abs Error')
+plt.plot(hist['epoch'], hist['mean_absolute_error'],
+         label='Train Error')
+plt.plot(hist['epoch'], hist['val_mean_absolute_error'],
+         label='Val Error')
+plt.legend()
+
+plt.figure()
+plt.xlabel('Epoch')
+plt.ylabel('Mean Square Error')
+plt.plot(hist['epoch'], hist['mean_squared_error'],
+         label='Train Error')
+plt.plot(hist['epoch'], hist['val_mean_squared_error'],
+         label='Val Error')
+plt.legend()
+plt.show()
 
 print(denorm(model.predict(test_X[-10:, :]), Y))
